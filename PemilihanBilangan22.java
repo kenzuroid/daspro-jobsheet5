c27f91cf0ad19827524282d58c2a3981cb60f490
kenzuroid <sholihahsitinikmatus@gmail.com>
2024-09-25 11:48:16 +0700

    perubahan 1

--- PemilihanBilangan22.java
+++ PemilihanBilangan22.java
@@ -2,19 +2,13 @@ import java.util.Scanner;
 public class PemilihanBilangan22 {
 
     public static void main(String[] args) {
-
         Scanner sc = new Scanner(System.in);
 
         System.out.print("Masukkan sebuah angka: ");
         int angka = sc.nextInt();
+        String hasil;
+        hasil = (angka % 2 == 0) ? " termasuk bilangan genap " : " termasuk bilangan ganjil ";
 
-        if (angka % 2 == 0)
-        {
-            System.out.println("Angka" + angka + " termasuk bilangan genapp");
-        }
-        else
-        {
-            System.out.println("Angka" + angka + " termasuk bilangan ganjil");
-        }
+        System.out.println( angka + hasil);
     }
 }
